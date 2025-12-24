5cf697d15cd745d6bb24512206679a2b2fc23fe1
LimboMusic <1817675972@qq.com>
2025-12-24 17:09:54 +0800

    feat: update auto reply

--- tasks/auto_reply.js
+++ tasks/auto_reply.js
@@ -3,30 +3,31 @@ import { sleep } from "../utils/utils.js";
 import { clickChatRedPoint } from "../modules/shop_data/shop_data.js";
 import { sendMessage } from "../modules/shop_data/shop_data.js";
 
-const url = 'https://www.goofish.com/im?spm=a21ybx.home.sidebar.2.4c053da6TfOP2U';
-const message = '来啦';
+const url =
+  "https://www.goofish.com/im?spm=a21ybx.home.sidebar.2.4c053da6TfOP2U";
+const message = "来啦";
 const USE_EXISTING_BROWSER = true;
 
 async function autoReply(url) {
-    const browser = new Browser();
-    if (USE_EXISTING_BROWSER) {
-        await browser.connectToExistingBrowser();
-    } else {
-        await browser.launchBrowser();
-    }
+  const browser = new Browser();
+  if (USE_EXISTING_BROWSER) {
+    await browser.connectToExistingBrowser();
+  } else {
+    await browser.launchBrowser();
+  }
+  await sleep(1000);
+  const page = await browser.navigateWithRetry(url);
+  let chatRedPointCount = await clickChatRedPoint(page);
+  while (chatRedPointCount > 0) {
+    console.log(`Chat red point count: ${chatRedPointCount}`);
     await sleep(1000);
-    const page = await browser.navigateWithRetry(url);
-    let chatRedPointCount = 0;
-    while (chatRedPointCount === 0) {
-        chatRedPointCount = await clickChatRedPoint(page);
-        console.log(`Chat red point count: ${chatRedPointCount}`);
-        await sleep(1000);
-        if (chatRedPointCount > 0) {
-            await sendMessage(page, message);
-            await sleep(1000);
-        }
+    if (chatRedPointCount > 0) {
+      await sendMessage(page, message);
+      await sleep(1000);
     }
-    await browser.closeBrowser();
+    chatRedPointCount = await clickChatRedPoint(page);
+  }
+  await browser.closeBrowser();
 }
 
-autoReply(url)
+autoReply(url);
