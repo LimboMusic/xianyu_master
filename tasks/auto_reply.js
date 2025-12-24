0b003d225357693395e5d5efddc3616ebb67ea6c
LimboMusic <1817675972@qq.com>
2025-12-24 16:53:53 +0800

    feat: add auto reply

--- tasks/auto_reply.js
+++ tasks/auto_reply.js
@@ -1,22 +1,28 @@
 import { Browser } from "../utils/browser.js";
 import { sleep } from "../utils/utils.js";
 import { clickChatRedPoint } from "../modules/shop_data/shop_data.js";
+import { sendMessage } from "../modules/shop_data/shop_data.js";
 
-const url = 'https://m.tb.cn/h.7VTdkIF?tk=WNzsfxauWsv';
+const url = 'https://www.goofish.com/im?spm=a21ybx.home.sidebar.2.4c053da6TfOP2U';
+const message = '来啦';
+const USE_EXISTING_BROWSER = true;
 
 async function autoReply(url) {
     const browser = new Browser();
-    await browser.launchBrowser();
-    const page = await browser.page;
-    await page.goto(url);
-    await page.waitForLoadState('networkidle');
+    if (USE_EXISTING_BROWSER) {
+        await browser.connectToExistingBrowser();
+    } else {
+        await browser.launchBrowser();
+    }
     await sleep(1000);
+    const page = await browser.navigateWithRetry(url);
     let chatRedPointCount = 0;
     while (chatRedPointCount === 0) {
         chatRedPointCount = await clickChatRedPoint(page);
+        console.log(`Chat red point count: ${chatRedPointCount}`);
         await sleep(1000);
         if (chatRedPointCount > 0) {
-            await sendMessage(page, '来啦');
+            await sendMessage(page, message);
             await sleep(1000);
         }
     }
